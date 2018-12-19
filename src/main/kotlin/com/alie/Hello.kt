eefe6c373e1c9388bfae7d2b1653667863fed44f
alie <jiiya07@gmail.com>
2018-12-19 12:12:57 +0800

    init commit

---
+++ src/main/kotlin/com/alie/Hello.kt
@@ -0,0 +1,13 @@
+package com.alie
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin")
+    val h = Human()
+    h.hello()
+}
+
+class Human {
+    fun hello(){
+        println("Hello Kotlin")
+    }
+}
